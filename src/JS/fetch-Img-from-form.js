db32abc37d5bb67d6e7e248f9d0b618d58114116
Evhenii Khartaniuk <co6alt@gmail.com>
2023-04-03 01:39:23 +0300

    11

--- src/JS/fetch-Img-from-form.js
+++ src/JS/fetch-Img-from-form.js
@@ -6,7 +6,7 @@ export async function fetchImg(formSubmitValue) {
     const BASE_URL = 'https://pixabay.com/api';
     const API = 'key=34903513-476090311f5800fa8542a8b0c';
     let currentPage = page;
-    page = 1;
+    currentPage = 1;
 
     return await axios.get(
       `${BASE_URL}/?${API}&q=${formSubmitValue}&image_type=photo&orientation=horizontal&safesearch=true&page=${currentPage}&per_page=40`
